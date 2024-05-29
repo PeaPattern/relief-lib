252cf0684470527bff4d6783227b5757affce2cc
PeaPattern <72760536+PeaPattern@users.noreply.github.com>
2024-05-28 20:08:38 -0400

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -447,7 +447,7 @@ local script = G2L["3"];
 		local NewList = ExampleList:Clone()
 		NewList.Parent = ModuleList
 		NewList.Title.Text = Name
-		NewList.Size = UDim2.new(0.07 * NewList.Title.Text:len(), 0, 0.03, 0)
+		NewList.Size = UDim2.new(0.09 * NewList.Title.Text:len(), 0, 0.03, 0)
 		NewList.Visible = false
 	
 		local NewModule = ExampleModule:Clone()
